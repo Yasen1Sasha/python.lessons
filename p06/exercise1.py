49808163535659a60763a24459689208d1880d90
Yasen1Sasha <sashayasinskij801@gmail.com>
2024-02-17 12:03:22 +0200

    lesoons

---
+++ p06/exercise1.py
@@ -0,0 +1,46 @@
+import random
+# my_list = [1, 'green', 148, 1488, 'dog', 40]
+# for item in my_list:
+#     print(item)
+
+# string = "Trali Vali"
+# print(string[0])
+# print(string[0])
+# for s in string:
+#     print(s)
+# number_list = [2, 6, 9, 0, 1]
+# print(number_list)
+#
+# number_list.append(88)
+# print(number_list)
+#
+#
+# l= len(number_list)
+# print(l)
+# random_number_list = []
+#
+# for j in range (21):
+#      n = random.randint(0,100)
+#      random_number_list.append(n)
+# sum = 0
+#
+# for i in random_number_list:
+#      sum += i
+#      print('\u2588'*i)
+#
+# print('Sum = ', sum)
+#
+#
+# print(random_number_list)
+random_number_list1 = []
+random_number_list2 = []
+
+for j in range (6):
+    n = random.randint(0,10)
+    random_number_list1.append(n)
+print(random_number_list1)
+
+for j in range (6):
+    n = random.randint(0,10)
+    random_number_list2.append(n)
+print(random_number_list2)
